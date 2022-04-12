2bc402421803c4f5b7e90dd4968362a0da721770
oont <oont@oonts-mbp.lan>
2022-04-12 01:18:45 -0500

    fix a typo

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,9 +10,9 @@ const AboutMe = (props) => {
       </div>
       <div className="center"></div>
       <p>
-    Apsiring software developer with a strong passion for learning and self growth. My primary languages include Java, Swift, and Python - however I am also familiar with JavaScript, C, and Bash.<br></br><br></br> 
+    Aspiring software developer with a strong passion for learning and self growth. <br></br>My primary languages include Java, Swift, and Python - however I am also familiar with JavaScript, C, and Bash.<br></br><br></br> 
     In my free time you can find me at the gym, outside cycling, or relaxing with friends.<br></br><br></br>
-    Currently, I am working on a passion driven project for iOS - RunePro. Please feel free to check it out on my GitHub and track my project!
+    Currently, I am working on a passion driven project for iOS - RunePro.
       </p>
     </div>
   );
